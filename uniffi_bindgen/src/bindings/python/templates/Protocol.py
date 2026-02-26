aeb4aeff5fda878973cd673c95225f8301286ff1
Daniel Granhão <32176319+danielgranhao@users.noreply.github.com>
2026-02-26 16:05:02 +0000

    Fix Python Protocol to use async def for async callback methods

--- uniffi_bindgen/src/bindings/python/templates/Protocol.py
+++ uniffi_bindgen/src/bindings/python/templates/Protocol.py
@@ -1,7 +1,7 @@
 class {{ protocol_name }}(typing.Protocol):
     {%- call py::docstring_value(protocol_docstring, 4) %}
     {%- for meth in methods.iter() %}
-    def {{ meth.name() }}(self, {% call py::arg_list_decl(meth) %}):
+    {% if meth.is_async() %}async {% endif %}def {{ meth.name() }}(self, {% call py::arg_list_decl(meth) %}):
         {%- call py::docstring(meth, 8) %}
         raise NotImplementedError
     {%- else %}
